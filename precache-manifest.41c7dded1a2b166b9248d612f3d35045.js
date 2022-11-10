e961e71f1194c5cda49548bb277c3959972bdc94
OlegTabachnikow <eskelll4ik@gmail.com>
2022-11-10 13:11:18 +0200

    Updates

---
+++ precache-manifest.41c7dded1a2b166b9248d612f3d35045.js
@@ -0,0 +1,93 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba9118b2dfa1308c8fb4",
+    "url": "/league-of-legends-db/static/css/main.2fa7e8fe.chunk.css"
+  },
+  {
+    "revision": "ba9118b2dfa1308c8fb4",
+    "url": "/league-of-legends-db/static/js/main.ba9118b2.chunk.js"
+  },
+  {
+    "revision": "f52c15243c2042f8d868",
+    "url": "/league-of-legends-db/static/js/1.f52c1524.chunk.js"
+  },
+  {
+    "revision": "9c02db117752e74b390f",
+    "url": "/league-of-legends-db/static/js/runtime~main.9c02db11.js"
+  },
+  {
+    "revision": "5cd814d47456209cd4b73623f4f4a5eb",
+    "url": "/league-of-legends-db/static/media/lol-logo.5cd814d4.png"
+  },
+  {
+    "revision": "e25ba05487ee161f7409d6259b6dce11",
+    "url": "/league-of-legends-db/static/media/gold.e25ba054.webp"
+  },
+  {
+    "revision": "23c4a0ef97a4919d52e2385aad112158",
+    "url": "/league-of-legends-db/static/media/search-icon.23c4a0ef.svg"
+  },
+  {
+    "revision": "90982da4bb2d3065c291cec160ed25b8",
+    "url": "/league-of-legends-db/static/media/mage.90982da4.svg"
+  },
+  {
+    "revision": "121101b317c31de8bbdcb0cb07d023e3",
+    "url": "/league-of-legends-db/static/media/tank.121101b3.svg"
+  },
+  {
+    "revision": "44f8b9f2572b173f768b344c1a9a9259",
+    "url": "/league-of-legends-db/static/media/marksman.44f8b9f2.svg"
+  },
+  {
+    "revision": "ffd527522de5c83282c8b014c671ff49",
+    "url": "/league-of-legends-db/static/media/support.ffd52752.svg"
+  },
+  {
+    "revision": "0a5742f907dc6c1285bbd0a3e06f0fbd",
+    "url": "/league-of-legends-db/static/media/fighter.0a5742f9.svg"
+  },
+  {
+    "revision": "1269f8f08b07ec4d5f517e45006c71ff",
+    "url": "/league-of-legends-db/static/media/assassin.1269f8f0.svg"
+  },
+  {
+    "revision": "7e0e123f3300781e58b69944b1c1b58c",
+    "url": "/league-of-legends-db/static/media/error-man.7e0e123f.png"
+  },
+  {
+    "revision": "a103d7312afcaa892d6cc8b26d3bc0a7",
+    "url": "/league-of-legends-db/static/media/league-of-legends-logo.a103d731.png"
+  },
+  {
+    "revision": "b895b4eef7cbcaf0d4c7e4892100010a",
+    "url": "/league-of-legends-db/static/media/ahri-picture.b895b4ee.png"
+  },
+  {
+    "revision": "ce64949dbddb2a172833546b6e02647b",
+    "url": "/league-of-legends-db/static/media/crystal-orb.ce64949d.png"
+  },
+  {
+    "url": "/league-of-legends-db/static/media/12420e8c141ca7c3dff41de2d59df13e.3ba61354.woff"
+  },
+  {
+    "url": "/league-of-legends-db/static/media/12420e8c141ca7c3dff41de2d59df13e.08a4423f.eot"
+  },
+  {
+    "url": "/league-of-legends-db/static/media/12420e8c141ca7c3dff41de2d59df13e.12420e8c.ttf"
+  },
+  {
+    "url": "/league-of-legends-db/static/media/12420e8c141ca7c3dff41de2d59df13e.c14737d5.woff2"
+  },
+  {
+    "url": "/league-of-legends-db/static/media/12420e8c141ca7c3dff41de2d59df13e.f2420436.svg"
+  },
+  {
+    "revision": "d0a56457c08176074525f0b7e1a23e1a",
+    "url": "/league-of-legends-db/static/media/jinx.d0a56457.png"
+  },
+  {
+    "revision": "e722102a7710f025cdfd12900e53cae9",
+    "url": "/league-of-legends-db/index.html"
+  }
+];
